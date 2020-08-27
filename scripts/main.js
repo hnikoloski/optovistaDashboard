6581e74630d33199905661b6e1cd85f88a0a2fae
Hristijan Nikoloski <h.nikoloski@outlook.com>
2020-08-27 16:20:50 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,88 @@
+$(function () {
+  $(".side-item").on("mouseover", function () {
+    $(".side-item").removeClass("active");
+    $(this).addClass("active");
+
+    $(this).on("mouseleave", function () {
+      $(this).removeClass("active");
+    });
+  });
+
+  $(".removeFavCard").on("mouseover", function () {
+    $(this).addClass("hovered");
+
+    $(this).on("mouseleave", function () {
+      $(this).removeClass("hovered");
+    });
+    $(".modal-card::before").css("opacity", "0.3");
+  });
+
+  // Home Slider Settings
+  $(".catalogCarousel").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: ` <div class="prevArrow"> <?xml version="1.0"encoding="utf-8"?> <svg version="1.1"id="Group_2263"width="21.78"height="40.43"xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink"x="0px"y="0px" viewBox="0 0 21.8 40.4"style="enable-background:new 0 0 21.8 40.4;"xml:space="preserve"> <style type="text/css"> .st0 {
+        fill:#676767;
+      }
+
+      </style> <path id="Path_2097"class="st0"d="M0.5,21.3L19.1,40c0.6,0.6,1.6,0.6,2.2,0c0.6-0.6,0.6-1.6,0-2.2L3.8,20.2L21.3,2.6
+c0.6-0.6, 0.6-1.6, 0-2.2c-0.6-0.6-1.6-0.6-2.2, 0L0.5, 19.1C-0.1, 19.7-0.1, 20.7, 0.5, 21.3z"/>
+</svg> </div> `,
+    nextArrow: ` <div class="nextArrow"> <svg id="Group_2263"data-name="Group 2263"xmlns="http://www.w3.org/2000/svg"width="21.78"height="40.43"viewBox="0 0 21.78 40.43"> <path id="Path_2097"data-name="Path 2097"d="M123.621,19.106,104.952.437a1.556,1.556,0,0,0-2.2,2.2l17.569,17.569L102.752,37.775a1.556,1.556,0,0,0,2.2,2.2l18.669-18.669A1.556,1.556,0,0,0,123.621,19.106Z"transform="translate(-102.297 0)"fill="#676767"/> </svg> </div> `,
+  });
+
+  // Notification
+  $(".hasNotification").on("click", function () {
+    $(".notificationWindow").slideToggle();
+    $(this).removeClass("hasNotification");
+  });
+  $(".userToolbar, .userFullName").on("click", function () {
+    $(".userInfo").slideToggle();
+  });
+  // Fav
+  $(".fav-modal-open").on("click", function (e) {
+    e.preventDefault();
+
+    $(".modal.right.fade .modal-dialog")
+      .css({
+        right: "-320px",
+      })
+      .animate(
+        {
+          right: "0px",
+        },
+        "slow"
+      );
+  });
+  // $(".collapse").addClass("show");
+});
+$(
+  ".content-wrapper section.ordini .ordersTable .toggleable i.ci.ci-arrowDown"
+).each(function (index) {
+  $(this).attr("data-target", "#id" + (index + 1));
+});
+$(".singleOrderDetails").each(function (index) {
+  $(this).attr("id", "id" + (index + 1));
+});
+// copy values
+$("#copyValues").on("click", function (e) {
+  e.preventDefault();
+  $("#sferoXs").val($("#sferoDx").val());
+  $("#cilindroXs").val($("#cilindroDx").val());
+  $("#asseXs").val($("#asseDx").val());
+  $("#diametroXs").val($("#diametroDx").val());
+  $("#addizioneXs").val($("#addizioneDx").val());
+  $("#altezzaXs").val($("#altezzaDx").val());
+});
+
+function getFileName(elm) {
+  var fn = $(elm).val();
+  var filename = fn.replace(/C:\\fakepath\\/i, "");
+  var label = $(elm).next();
+  if (fn != "") {
+    $(label).text(filename);
+    $(label).css("border", "2px solid #276462");
+  }
+}
---
+++ scripts/steps.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  // SmartWizard initialize
+  $("#smartwizard").smartWizard({
+    justified: true,
+    toolbarSettings: {
+      toolbarPosition: "bottom", // none, top, bottom, both
+      toolbarButtonPosition: "center", // left, right, center
+      showNextButton: false, // show/hide a Next button
+      showPreviousButton: false, // show/hide a Previous button
+      toolbarExtraButtons: [
+        $("<button></button>")
+          .text("Reset")
+          .addClass("btn btn-info")
+          .on("click", function () {
+            window.location.href = "";
+          }),
+        $("<button></button>").text("Avanti").addClass("btn sw-btn-next"),
+      ], // Extra buttons to show on toolbar, array of jQuery input/buttons elements
+    },
+    anchorSettings: {
+      anchorClickable: true, // Enable/Disable anchor navigation
+      enableAllAnchors: false, // Activates all anchors clickable all times
+      markDoneStep: true, // Add done state on navigation
+      markAllPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
+      removeDoneStepOnNavigateBack: false, // While navigate back done step after active step will be cleared
+      enableAnchorOnDoneStep: true, // Enable/Disable the done steps navigation
+    },
+    transition: {
+      animation: "slide-horizontal", // Effect on navigation, none/fade/slide-horizontal/slide-vertical/slide-swing
+      speed: "4000", // Transion animation speed
+      easing: "", // Transition animation easing. Not supported without a jQuery easing plugin
+    },
+    autoAdjustHeight: true, // Automatically adjust content height
+    theme: "dots",
+    lang: {
+      // Language variables for button
+      next: "Avanti",
+      previous: "Previous",
+    },
+  });
+
+  $(".sw-btn-next").on("click", function () {
+    $(".step-wrapper").has(".done").addClass("done");
+    let totalPrice = 0;
+
+    totalPrice = parseInt($(".singleProductPrice").text());
+
+    $("#totalPrice").text(totalPrice);
+  });
+});
